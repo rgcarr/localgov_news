391a0c8b122f532a75e6c0cd9106d0c906111808
Dan Champion <support@championinternet.com>
2021-01-27 12:44:00 +0000

    Replace \Drupal calls with DI

--- modules/localgov_newsroom/src/Controller/NewsroomController.php
+++ modules/localgov_newsroom/src/Controller/NewsroomController.php
@@ -3,7 +3,11 @@
 namespace Drupal\localgov_newsroom\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Zend\Diactoros\Response\JsonResponse;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\localgov_newsroom\Newsroom;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class NewsroomController.
@@ -11,26 +15,69 @@ use Zend\Diactoros\Response\JsonResponse;
  * @package Drupal\localgov_newsroom\Controller
  */
 class NewsroomController extends ControllerBase {
+  /**
+   * Newsroom.
+   *
+   * @var \Drupal\localgov_newsroom\Newsroom
+   */
+  private $newsroom;
 
   /**
-   * @return \Zend\Diactoros\Response\JsonResponse
+   * Core request_stack service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  private $requestStack;
+
+  /**
+   * Renderer interface.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  private $renderer;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(Newsroom $newsroom, RequestStack $request_stack, RendererInterface $renderer) {
+    $this->newsroom = $newsroom;
+    $this->requestStack = $request_stack;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('localgov_newsroom.newsroom'),
+      $container->get('request_stack'),
+      $container->get('renderer')
+    );
+  }
+
+  /**
+   * Ajax callback.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   JSON response
    */
   public function ajaxCallback() {
     $results = [];
 
-    foreach (\Drupal::service('localgov_newsroom.newsroom')->getPage(\Drupal::request()->get('page')) as $node) {
+    foreach ($this->newsroom->getPage($this->requestStack->getCurrentRequest()->get('page')) as $node) {
       $render_array = $this->entityTypeManager()
         ->getViewBuilder('node')
         ->view($node, 'teaser');
 
-      $results[] = \Drupal::service('renderer')
+      $results[] = $this->renderer
         ->render($render_array);
     }
 
     return new JsonResponse([
       'html' => implode('', $results),
-      'totalItems' => \Drupal::service('localgov_newsroom.newsroom')->getCount(),
-      'finished' => ((\Drupal::request()->get('page') + 1) * 10) >= \Drupal::service('localgov_newsroom.newsroom')->getCount(),
+      'totalItems' => $this->newsroom->getCount(),
+      'finished' => (($this->requestStack->getCurrentRequest()->get('page') + 1) * 10) >= $this->newsroom->getCount(),
     ]);
   }
 
