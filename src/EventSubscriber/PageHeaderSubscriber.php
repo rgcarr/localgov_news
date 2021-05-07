d14ecbb73b2c32fbe5c6baf179cee7db713b87de
Dan Champion <support@championinternet.com>
2021-05-07 08:45:19 +0100

    Fix coding standards

--- src/EventSubscriber/PageHeaderSubscriber.php
+++ src/EventSubscriber/PageHeaderSubscriber.php
@@ -13,23 +13,24 @@ use Drupal\localgov_core\Event\PageHeaderDisplayEvent;
  */
 class PageHeaderSubscriber implements EventSubscriberInterface {
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function getSubscribedEvents() {
-        return [
-            PageHeaderDisplayEvent::EVENT_NAME => ['setPageHeader', 0],
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      PageHeaderDisplayEvent::EVENT_NAME => ['setPageHeader', 0],
+    ];
+  }
 
-    /**
-     * Hide page header block.
-     */
-    public function setPageHeader(PageHeaderDisplayEvent $event) {
-        if ($event->getEntity() instanceof Node &&
-            ($event->getEntity()->bundle() == 'localgov_news_article')
-        ) {
-            $event->setVisibility(FALSE);
-        }
+  /**
+   * Hide page header block.
+   */
+  public function setPageHeader(PageHeaderDisplayEvent $event) {
+    if ($event->getEntity() instanceof Node &&
+          ($event->getEntity()->bundle() == 'localgov_news_article')
+      ) {
+      $event->setVisibility(FALSE);
     }
+  }
+
 }
